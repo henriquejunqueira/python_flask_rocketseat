1aad4ae510f43bd8ee4d8d1eb42affdeeaa138d9
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-08-24 23:24:50 -0300

    Criada rota e função para cadastrar um produto

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask # Importa a biblioteca Flask
+from flask import Flask, request, jsonify # Importa as bibliotecas Flask, request e jsonify
 from flask_sqlalchemy import SQLAlchemy # Importa a biblioteca para trabahar com SQLite 
 
 app = Flask(__name__) # Cria uma instancia do aplicativo
@@ -17,6 +17,16 @@ class Product(db.Model):
 @app.route('/') # Define uma rota raiz (página inicial) e a função que será executada quando o usuário requisitar
 def hello_world(): # Define a função que a rota raiz irá executar
     return 'Hello World'
+
+@app.route('/api/products/add', methods=["POST"]) # Define a rota de adição de produtos e o metodo como POST
+def add_product():
+    data = request.json # Pega os dados na forma de json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))# Cria o produto
+        db.session.add(product) # Cria a sessão de adição do produto
+        db.session.commit() # Envia os dados para o banco
+        return jsonify({"message": "Product added successfully"})
+    return jsonify({'message': "Invalid product data"}), 400 # Retorna a resposta de erro como json e código de erro 400
 
 if __name__ == "__main__": # Verifica se o arquivo está sendo executado diretamente ou importado por outro arquivo
     app.run(debug=True) # Executa a aplicação utilizando o modo debug
