bad5778444505fd7d2f8d6b13c7d810bf55a69fd
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-08-24 22:27:22 -0300

    Definido local do banco de dados, iniciada conexão com o banco de dados, criada classe Produto

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask # Importa a biblioteca Flask
+from flask_sqlalchemy import SQLAlchemy # Importa a biblioteca para trabahar com SQLite 
 
 app = Flask(__name__) # Cria uma instancia do aplicativo
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db' # Define o local do banco de dados
+
+db = SQLAlchemy(app) # Inicia a conexão com o banco de dados
+
+# Cria uma classe Produto recebendo como parâmetro um model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # Define a coluna id do banco de dados
+    name = db.Column(db.String(120), nullable=False) # Define a coluna nome do banco de dados
+    price = db.Column(db.Float, nullable=False) # Define a coluna preço do banco de dados
+    description = db.Column(db.Text, nullable=True)
 
 # Cria as rotas para comunicar com a API
 @app.route('/') # Define uma rota raiz (página inicial) e a função que será executada quando o usuário requisitar
