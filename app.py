4288589137acb058b5d49bf9f049180cbc2b1d38
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-08-25 13:14:52 -0300

    Criada rota e função de remoção de dados

--- app.py
+++ app.py
@@ -19,14 +19,24 @@ def hello_world(): # Define a função que a rota raiz irá executar
     return 'Hello World'
 
 @app.route('/api/products/add', methods=["POST"]) # Define a rota de adição de produtos e o metodo como POST
-def add_product():
+def add_product(): # Define a função que a rota de adição irá executar
     data = request.json # Pega os dados na forma de json
     if 'name' in data and 'price' in data:
         product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))# Cria o produto
         db.session.add(product) # Cria a sessão de adição do produto
-        db.session.commit() # Envia os dados para o banco
-        return jsonify({"message": "Product added successfully"})
+        db.session.commit() # Envia os dados para o banco de dados
+        return jsonify({"message": "Product added successfully"}) # Retorna a mensagem de sucesso como json
     return jsonify({'message': "Invalid product data"}), 400 # Retorna a resposta de erro como json e código de erro 400
+
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"]) # Define a rota de remoção de produtos e o metodo como DELETE
+def delete_product(product_id): # Define a função que a rota de remoção irá executar
+    product = Product.query.get(product_id) # Recupera o produto do banco de dados
+    # Verifica se o produto existe
+    if product: # Se exite, o dado é removido do banco de dados
+        db.session.delete(product) # Cria a sessão de remoção do produto
+        db.session.commit() # Remove os dados do banco de dados
+        return jsonify({"message": "Product deleted successfully"}) # Retorna a mensagem de sucesso como json
+    return jsonify({"message": "Product not found"}) , 404 # Se não existe, retorna o erro 404 not found
 
 if __name__ == "__main__": # Verifica se o arquivo está sendo executado diretamente ou importado por outro arquivo
     app.run(debug=True) # Executa a aplicação utilizando o modo debug
